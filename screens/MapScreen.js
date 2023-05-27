8b99451e841bb4983daa47f874fa7be7ee49af8f
Emsun <emsunconcept@gmail.com>
2023-05-27 17:02:29 +0100

    started the Uber clone

---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MapScreen = () => {
+	return (
+		<View>
+			<Text>Here is the map screen</Text>
+		</View>
+	);
+};
+
+export default MapScreen;
